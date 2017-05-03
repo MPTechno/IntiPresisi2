7b615336f17c4a591799fc9728b30afc79686c79
Anand Patel <flt.anandkumar@gmail.com>
2017-05-03 14:43:17 +0530

    [IMP] set code for the unicode character in the Sale Order export .XML file

--- convert_to_txt_and_xml/models/sale.py
+++ convert_to_txt_and_xml/models/sale.py
@@ -5,6 +5,7 @@ import base64
 from xml.etree import ElementTree
 from xml.etree.ElementTree import Element,SubElement
 from xml.dom import minidom
+from odoo.tools.misc import ustr
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
@@ -15,58 +16,58 @@ class SaleOrder(models.Model):
             Orders = Element('Orders')#Top Element
             for order in self.env['sale.order'].browse(order_ids):
                 Order = SubElement(Orders,'Order')
-                Order.set('OrderNumber',str(order.name))
+                Order.set('OrderNumber',ustr(order.name))
                 
                 head = SubElement(Order,'head')#Header Part
                 
                 supplier = SubElement(head, 'Supplier')#Supplier (Company Address)
                 supplier.set('SupplierCodeEdi','123')#FixMe
                 supplier_name = SubElement(supplier, 'Name')
-                supplier_name.text = str(order.user_id.company_id.name)
+                supplier_name.text = ustr(order.user_id.company_id.name)
                 
                 StreetBox1 = SubElement(supplier, 'StreetBox1')
                 if order.user_id.company_id.street:
-                    StreetBox1.text = str(order.user_id.company_id.street)
+                    StreetBox1.text = ustr(order.user_id.company_id.street)
                     
                 StreetBox2 = SubElement(supplier, 'StreetBox2')
                 if order.user_id.company_id.street2:
-                    StreetBox2.text = str(order.user_id.company_id.street2)
+                    StreetBox2.text = ustr(order.user_id.company_id.street2)
                     
                 ZipCity1 = SubElement(supplier, 'ZipCity1')
                 if order.user_id.company_id.city:
-                    ZipCity1.text = str(order.user_id.company_id.city) +' ' + str(order.user_id.company_id.state_id.name or '')
+                    ZipCity1.text = ustr(order.user_id.company_id.city) +' ' + ustr(order.user_id.company_id.state_id.name or '')
                     
                 ZipCity2 = SubElement(supplier, 'ZipCity2')
                 if order.user_id.company_id.zip:
-                    ZipCity2.text = str(order.user_id.company_id.zip)
+                    ZipCity2.text = ustr(order.user_id.company_id.zip)
                     
                 Country = SubElement(supplier, 'Country')
                 if order.user_id.company_id.country_id:
-                    Country.text = str(order.user_id.company_id.country_id.name)
+                    Country.text = ustr(order.user_id.company_id.country_id.name)
                     
                 Buyer = SubElement(head, 'Buyer')#Customer Address
                 Buyer_name = SubElement(Buyer, 'Name')
-                Buyer_name.text = str(order.partner_id.name)
+                Buyer_name.text = ustr(order.partner_id.name)
                 
                 StreetBox1 = SubElement(Buyer, 'StreetBox1')
                 if order.partner_id.street:
-                    StreetBox1.text = str(order.partner_id.street)
+                    StreetBox1.text = ustr(order.partner_id.street)
                     
                 StreetBox2 = SubElement(Buyer, 'StreetBox2')
                 if order.user_id.company_id.street2:
-                    StreetBox2.text = str(order.partner_id.street2)
+                    StreetBox2.text = ustr(order.partner_id.street2)
                     
                 ZipCity1 = SubElement(Buyer, 'ZipCity1')
                 if order.user_id.company_id.city:
-                    ZipCity1.text = str(order.partner_id.city) +' ' + str(order.partner_id.state_id.name or '')
+                    ZipCity1.text = ustr(order.partner_id.city) +' ' + ustr(order.partner_id.state_id.name or '')
                     
                 ZipCity2 = SubElement(Buyer, 'ZipCity2')
                 if order.partner_id.zip:
-                    ZipCity2.text = str(order.partner_id.zip)
+                    ZipCity2.text = ustr(order.partner_id.zip)
                     
                 Country = SubElement(Buyer, 'Country')
                 if order.partner_id.country_id:
-                    Country.text = str(order.partner_id.country_id.name)
+                    Country.text = ustr(order.partner_id.country_id.name)
                     
                 References = SubElement(head, 'References')#References    
                 BuyerReference = SubElement(References, 'BuyerReference')
@@ -79,82 +80,82 @@ class SaleOrder(models.Model):
                 
                 DeliveryAddress = SubElement(head, 'DeliveryAddress')#Delivery Address #FixMe: Need to confirm which value?
                 DeliveryAddress_name = SubElement(DeliveryAddress, 'Name')
-                DeliveryAddress_name.text = str(order.partner_shipping_id.name)
+                DeliveryAddress_name.text = ustr(order.partner_shipping_id.name)
                 
                 StreetBox1 = SubElement(DeliveryAddress, 'StreetBox1')
                 if order.partner_shipping_id.street:
-                    StreetBox1.text = str(order.partner_shipping_id.street)
+                    StreetBox1.text = ustr(order.partner_shipping_id.street)
                     
                 StreetBox2 = SubElement(DeliveryAddress, 'StreetBox2')
                 if order.user_id.company_id.street2:
-                    StreetBox2.text = str(order.partner_shipping_id.street2)
+                    StreetBox2.text = ustr(order.partner_shipping_id.street2)
                     
                 ZipCity1 = SubElement(DeliveryAddress, 'ZipCity1')
                 if order.user_id.company_id.city:
-                    ZipCity1.text = str(order.partner_shipping_id.city) +' ' + str(order.partner_shipping_id.state_id.name or '')
+                    ZipCity1.text = ustr(order.partner_shipping_id.city) +' ' + ustr(order.partner_shipping_id.state_id.name or '')
                     
                 ZipCity2 = SubElement(DeliveryAddress, 'ZipCity2')
                 if order.partner_shipping_id.zip:
-                    ZipCity2.text = str(order.partner_shipping_id.zip)
+                    ZipCity2.text = ustr(order.partner_shipping_id.zip)
                     
                 Country = SubElement(DeliveryAddress, 'Country')
                 if order.partner_shipping_id.country_id:
-                    Country.text = str(order.partner_shipping_id.country_id.name)
+                    Country.text = ustr(order.partner_shipping_id.country_id.name)
                 CompanyAdressFlag = SubElement(DeliveryAddress, 'CompanyAdressFlag')#FixMe
                 
                 Terms = SubElement(head, 'Terms')#Terms
                 DeliveryTerms = SubElement(Terms, 'DeliveryTerms')
                 IncoTermCombiTerm = SubElement(DeliveryTerms, 'IncoTermCombiTerm')
                 if order.incoterm:
-                    IncoTermCombiTerm.text = str(order.incoterm.name)
+                    IncoTermCombiTerm.text = ustr(order.incoterm.name)
                 DeliveryMethod = SubElement(DeliveryTerms, 'DeliveryMethod')
                 TransportPayer = SubElement(DeliveryTerms, 'TransportPayer')
                 CustomerTransportTimeDays = SubElement(DeliveryTerms, 'CustomerTransportTimeDays')
                 CustomerInvoiceCode = SubElement(Terms, 'CustomerInvoiceCode')
                 OrderDate = SubElement(Terms, 'OrderDate')
                 if order.date_order:
-                    OrderDate.text = str(order.date_order).split(' ')[0]
+                    OrderDate.text = ustr(order.date_order).split(' ')[0]
                     
                 PaymentTerms = SubElement(Terms, 'PaymentTerms')
                 
                 TermsOfPaymentDays = SubElement(PaymentTerms, 'TermsOfPaymentDays')
                 if order.payment_term_id:
-                    TermsOfPaymentDays.text = str(order.payment_term_id.name)
+                    TermsOfPaymentDays.text = ustr(order.payment_term_id.name)
                     
                 Export = SubElement(head, 'Export')#Export
                 Currency = SubElement(Export, 'Currency')
                 if order.pricelist_id.currency_id:
-                    Currency.text = str(order.pricelist_id.currency_id.name)
+                    Currency.text = ustr(order.pricelist_id.currency_id.name)
                 Rows = SubElement(Order,'Rows')#Line Items
                 line_no = 1
                 for line in order.order_line:
                     Row = SubElement(Rows,'Row')
-                    Row.set('RowNumber',str(line_no))
+                    Row.set('RowNumber',ustr(line_no))
                     line_no+=1
                     Row.set('RowType','')#FixMe
                     
                     Part = SubElement(Row,'Part')
                     if line.product_id and line.product_id.part_number:
-                        Part.set('PartNumber',str(line.product_id.part_number))
+                        Part.set('PartNumber',ustr(line.product_id.part_number))
                     else:
                         Part.set('PartNumber','')
                     if line.product_id and line.product_id.customer_part_no:
-                        Part.set('SupplierPartNumber',str(line.product_id.customer_part_no))
+                        Part.set('SupplierPartNumber',ustr(line.product_id.customer_part_no))
                     else:
                         Part.set('SupplierPartNumber','')
                     
                     Text = SubElement(Row,'Text')
-                    Text.text = str(line.name or line.product_id.name)
+                    Text.text = ustr(line.name or line.product_id.name)
                     
                     ReferenceNumber = SubElement(Row,'ReferenceNumber')
                     if line.product_id.default_code:
-                        ReferenceNumber.text = str(line.product_id.default_code)
+                        ReferenceNumber.text = ustr(line.product_id.default_code)
                     
                     Quantity = SubElement(Row,'Quantity')
-                    Quantity.text = str(line.product_uom_qty)
+                    Quantity.text = ustr(line.product_uom_qty)
                     
                     Unit = SubElement(Row,'Unit')
-                    Unit.text = str(line.product_uom.name)
+                    Unit.text = ustr(line.product_uom.name)
                     
                     DeliveryPeriod = SubElement(Row,'DeliveryPeriod')
                     DeliveryPeriod.text = ''#FixMe
@@ -173,11 +174,12 @@ class SaleOrder(models.Model):
                     
             filename = 'Sale Order XML.xml'
             f = open(filename, 'w')
-            data = minidom.parseString(ElementTree.tostring(Orders)).toprettyxml()
+            orders = ElementTree.tostring(Orders,encoding="utf-8")
+            data = minidom.parseString(orders).toprettyxml(encoding="utf-8")
             f.write(data)
             f.close()
             file_base64 = ''
-            with open(filename, "r") as file:
+            with open(filename, "r",) as file:
                 file_base64 = base64.b64encode(file.read())
             export_id = self.env['so.xml.file'].create({
                                                     'xml_file': file_base64,
