b47aa4a3db7e84c07c9ac611bc09c155239cc2e6
MpTechnolabsOdoo <odoodevelopers@gmail.com>
2017-07-12 12:31:16 +0530

    [ADD] Export To Excel Product.product

--- convert_to_txt_and_xml/models/product.py
+++ convert_to_txt_and_xml/models/product.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo.tools.float_utils import float_is_zero, float_compare
+from odoo.exceptions import UserError, AccessError
+from odoo.tools.misc import formatLang
+from odoo.addons.base.res.res_partner import WARNING_MESSAGE, WARNING_HELP
+import odoo.addons.decimal_precision as dp
+
 import base64
+import StringIO
+import xlsxwriter
+import csv
+import os.path
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
@@ -37,3 +51,159 @@ class ProductProduct(models.Model):
                     'context': self._context,
                     'target': 'new',
                     }
+
+
+    def download_excel(self):
+        product_ids = self._context.get('active_ids',[])
+        if product_ids:
+            res = self
+            output =  StringIO.StringIO()
+            workbook = xlsxwriter.Workbook(output, {'in_memory': True})
+            worksheet = workbook.add_worksheet()
+            row = 0
+            col = 0
+            bold_format = workbook.add_format({'bold':  1})
+            right_format = workbook.add_format({'bold':1,'align':'right'})
+            merge_format = workbook.add_format({'bold': 1,'border': 1,'align': 'center','valign': 'vcenter'})
+            
+            worksheet.write(row, col,  unicode('Part Number', "utf-8"), bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            worksheet.write(row, col, unicode('Name', "utf-8"), bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            worksheet.write(row, col,unicode('Part Type', "utf-8") , bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            worksheet.write(row, col, unicode('Unit', "utf-8"), bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            worksheet.write(row, col, unicode('Alternate Unit', "utf-8"), bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            worksheet.write(row, col, unicode('Part Code', "utf-8"), bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            worksheet.write(row, col, unicode('Product Group', "utf-8"), bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            worksheet.write(row, col, unicode('Product Category', "utf-8"), bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            worksheet.write(row, col, unicode('Drawing Number', "utf-8"), bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            worksheet.write(row, col, unicode('Revision', "utf-8"), bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            worksheet.write(row, col, unicode('Add Name 1', "utf-8"), bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            worksheet.write(row, col, unicode('Add Name 2', "utf-8"), bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            worksheet.write(row, col, unicode('Add Name 3', "utf-8"), bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            worksheet.write(row, col, unicode('Add Name 4', "utf-8"), bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            worksheet.write(row, col, unicode('Customer Code', "utf-8"), bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            worksheet.write(row, col, unicode('Customer Price', "utf-8"), bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            worksheet.write(row, col, unicode('Customer Part No', "utf-8"), bold_format)
+            worksheet.set_column(row, col, 20)
+            col += 1
+
+            row += 1
+            for product in product_ids:
+                product_obj = self.env['product.product'].browse(product)
+                row += 1
+                col = 0
+
+                worksheet.write(row, col,unicode((product_obj.part_number or '').encode("utf-8"), "utf-8"))
+                col += 1
+                
+                worksheet.write(row, col,unicode((product_obj.name or '').encode("utf-8"), "utf-8"))
+                col += 1
+
+                worksheet.write(row, col,unicode((product_obj.part_type_id.name or '').encode("utf-8"), "utf-8"))
+                col += 1
+
+                worksheet.write(row, col,unicode((product_obj.uom_id.name or '').encode("utf-8"), "utf-8"))
+                col += 1
+
+                worksheet.write(row, col,unicode((product_obj.alternative_uom_id.name or '').encode("utf-8"), "utf-8"))
+                col += 1
+
+                worksheet.write(row, col,unicode((product_obj.part_code.name or '').encode("utf-8"), "utf-8"))
+                col += 1
+
+                worksheet.write(row, col,unicode((product_obj.product_group.name or '').encode("utf-8"), "utf-8"))
+                col += 1
+
+                worksheet.write(row, col,unicode((product_obj.categ_id.name or '').encode("utf-8"), "utf-8"))
+                col += 1
+                
+                worksheet.write(row, col,unicode((product_obj.drawing_no or '').encode("utf-8"), "utf-8"))
+                col += 1
+
+                worksheet.write(row, col,unicode((product_obj.revision or '').encode("utf-8"), "utf-8"))
+                col += 1
+
+                worksheet.write(row, col,unicode((product_obj.add_name_1 or '').encode("utf-8"), "utf-8"))
+                col += 1
+
+                worksheet.write(row, col,unicode((product_obj.add_name_2 or '').encode("utf-8"), "utf-8"))
+                col += 1
+
+                worksheet.write(row, col,unicode((product_obj.add_name_3 or '').encode("utf-8"), "utf-8"))
+                col += 1
+
+                worksheet.write(row, col,unicode((product_obj.add_name_4 or '').encode("utf-8"), "utf-8"))
+                col += 1
+
+                worksheet.write(row, col,unicode((product_obj.customer_code.name or '').encode("utf-8"), "utf-8"))
+                col += 1
+
+                worksheet.write(row, col,unicode((product_obj.lst_price or '').encode("utf-8"), "utf-8"))
+                col += 1
+
+                worksheet.write(row, col,unicode((product_obj.customer_part_no or '').encode("utf-8"), "utf-8"))
+                col += 1
+
+            row += 2
+            row += 1
+
+            workbook.close()
+            output.seek(0)
+            result = base64.b64encode(output.read())
+            attachment_obj = self.env['ir.attachment']
+            attachment_id = attachment_obj.create({'name': 'Product Excel.xlsx', 'datas_fname': 'Product Excel.xlsx', 'datas': result})
+            download_url = '/web/content/' + str(attachment_id.id) + '?download=true'
+            base_url = self.env['ir.config_parameter'].get_param('web.base.url')
+
+            return {
+                "type": "ir.actions.act_url",
+                "url": str(base_url) + str(download_url),
+                "target": "self",
+            }
